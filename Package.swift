666a2d59c3caacff590ac930022ccfe59feafab9
jiawei <kmiori@163.com>
2020-04-04 02:28:45 +0800

    Initial SwiftLibssh

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftLibssh",
+    pkgConfig: "libssh",
+    providers: [
+        .brew(["libssh"]),
+        .apt(["libssh-dev"])
+    ]
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftLibssh
+
+A description of this package.
