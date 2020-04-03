6676a15b4f9fce8a53d6e359f671b14cb2a546e9
jiawei <kmiori@163.com>
2020-04-04 03:01:22 +0800

    fix Package.swift syntax error

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     providers: [
         .brew(["libssh"]),
         .apt(["libssh-dev"])
-    ]
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
